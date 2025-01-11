349954d6c2392098b0b01e2d501321e7c009fb2d
Dipak Bachhav <118241898+DipakBachhav25@users.noreply.github.com>
2025-01-11 20:29:02 +0530

    Time: 27 ms (32.01%), Space: 45.7 MB (19.42%) - LeetHub

---
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canConstruct(String s, int k) {
+
+        if(s.length() < k) return false;
+
+        HashMap<Character, Integer> charCount = new HashMap<>();
+        for(char c : s.toCharArray()){
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
+
+        int oddCount = 0;
+        for(int count : charCount.values()){
+            if(count%2 != 0){
+                oddCount++;
+            }
+        }
+
+        return k <= s.length() && oddCount <= k;
+    }
+}
