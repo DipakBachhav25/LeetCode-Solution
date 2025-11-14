fac783fe35fbfa54ab5df75da924b21db1533442
Dipak Bachhav <118241898+DipakBachhav25@users.noreply.github.com>
2025-11-14 19:45:56 +0530

    Time: 10 ms (85.09%), Space: 83.4 MB (6.14%) - LeetHub

--- 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.java
+++ 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.java
@@ -1,17 +1,32 @@
 class Solution {
     public int[][] rangeAddQueries(int n, int[][] queries) {
-        int[][] prefixSum = new int[n][n];
+        int[][] prefixSum = new int[n+1][n+1];
         for(int[] query : queries){
             int row1 = query[0], row2 = query[2];
             int col1 = query[1], col2 = query[3];
-
-            for(int i=row1; i<=row2; i++){
-                for(int j=col1; j<=col2; j++){
-                    prefixSum[i][j]++;
-                }
-            } 
+            prefixSum[row1][col1]++;
+            prefixSum[row1][col2+1]--;
+            prefixSum[row2+1][col1]--;
+            prefixSum[row2+1][col2+1]++;
         }
 
-        return prefixSum;
+        for(int i=0; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                prefixSum[i][j] += prefixSum[i][j-1];
+            }
+        }
+
+        for(int j=0; j<=n; j++){
+            for(int i=1; i<=n; i++){
+                prefixSum[i][j] += prefixSum[i-1][j];
+            }
+        }
+
+        int[][] ans = new int[n][n];
+        for(int i=0; i<n; i++){
+            System.arraycopy(prefixSum[i], 0, ans[i], 0, n);
+        }
+
+        return ans;
     }
 }
