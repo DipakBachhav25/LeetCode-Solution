26a452affd0b7d526b2f6cf4455578817fa027d7
Dipak Bachhav <118241898+DipakBachhav25@users.noreply.github.com>
2025-12-01 21:01:03 +0530

    Time: 16 ms (71.43%), Space: 73.8 MB (37.36%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.java
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.java
@@ -1,44 +1,44 @@
 class Solution {
-    // public long maxRunTime(int n, int[] batteries) {
-    //     long totalMins = 0L;
-    //     for(int x : batteries) totalMins += x;
-
-    //     long l = 0L, r = totalMins/n;
-    //     while(l <= r){
-    //         long mid = l + (r-l)/2;
-    //         if(canRunComputers(n, batteries, mid)) l = mid+1;
-    //         else r = mid-1;
-    //     }
-
-    //     return r;
-    // }
-
-    // public static boolean canRunComputers(int n, int[] batteries, long time){
-    //     if(time == 0) return true;
-    //     long req = time * (long) n;
-    //     long s = 0L;
-
-    //     for(int b : batteries){
-    //         s += Math.min((long) b, time);
-    //         if(s >= req) return true;
-    //     }
-
-    //     return s >= req;
-    // }
-
-    public long maxRunTime(int n, int[] batteries){
-        Arrays.sort(batteries);
+    public long maxRunTime(int n, int[] batteries) {
         long totalMins = 0L;
         for(int x : batteries) totalMins += x;
 
-        for(int i=batteries.length-1; i>=0; i--){
-            if(batteries[i] > totalMins/n){
-                totalMins -= batteries[i];
-                n--;
-            }else{
-                return totalMins/n;
-            }
+        long l = 0L, r = totalMins/n;
+        while(l <= r){
+            long mid = l + (r-l)/2;
+            if(canRunComputers(n, batteries, mid)) l = mid+1;
+            else r = mid-1;
         }
-        return totalMins/n;
+
+        return r;
     }
+
+    public static boolean canRunComputers(int n, int[] batteries, long time){
+        if(time == 0) return true;
+        long req = time * (long) n;
+        long s = 0L;
+
+        for(int b : batteries){
+            s += Math.min((long) b, time);
+            if(s >= req) return true;
+        }
+
+        return s >= req;
+    }
+
+    // public long maxRunTime(int n, int[] batteries){
+    //     Arrays.sort(batteries);
+    //     long totalMins = 0L;
+    //     for(int x : batteries) totalMins += x;
+
+    //     for(int i=batteries.length-1; i>=0; i--){
+    //         if(batteries[i] > totalMins/n){
+    //             totalMins -= batteries[i];
+    //             n--;
+    //         }else{
+    //             return totalMins/n;
+    //         }
+    //     }
+    //     return totalMins/n;
+    // }
 }
