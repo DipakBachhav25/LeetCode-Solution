5b1a8e4550e159c67038c494bf52ac5db43dd1a1
Dipak Bachhav <118241898+DipakBachhav25@users.noreply.github.com>
2025-12-01 20:54:07 +0530

    Time: 16 ms (71.43%), Space: 74.1 MB (17.03%) - LeetHub

---
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.java
@@ -0,0 +1,28 @@
+class Solution {
+    public long maxRunTime(int n, int[] batteries) {
+        long totalMins = 0L;
+        for(int x : batteries) totalMins += x;
+
+        long l = 0L, r = totalMins/n;
+        while(l <= r){
+            long mid = l + (r-l)/2;
+            if(canRunComputers(n, batteries, mid)) l = mid+1;
+            else r = mid-1;
+        }
+
+        return r;
+    }
+
+    public static boolean canRunComputers(int n, int[] batteries, long time){
+        if(time == 0) return true;
+        long req = time * (long) n;
+        long s = 0L;
+
+        for(int b : batteries){
+            s += Math.min((long) b, time);
+            if(s >= req) return true;
+        }
+
+        return s >= req;
+    }
+}
