618701a031df0371d6f4f6677d27074ebaf8362d
Dipak Bachhav <118241898+DipakBachhav25@users.noreply.github.com>
2025-12-10 12:53:24 +0530

    Time: 2 ms (74.67%), Space: 92.1 MB (26.67%) - LeetHub

--- 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.java
+++ 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.java
@@ -2,13 +2,9 @@ class Solution {
     public int countPermutations(int[] complexity) {
         int MOD = 1_000_000_007;
 
-        if (Arrays.stream(complexity, 1, complexity.length)
-                  .anyMatch(x -> x <= complexity[0])) {
-            return 0;
-        }
-
         long ans = 1;
         for (int i = 1; i < complexity.length; i++) {
+            if(complexity[i] <= complexity[0]) return 0;
             ans = (ans * i) % MOD;
         }
 
