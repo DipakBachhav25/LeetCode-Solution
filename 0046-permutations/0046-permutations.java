8acdbd1f93df2dc67bdad7ea3eaec0f9d2887437
Dipak Bachhav <118241898+DipakBachhav25@users.noreply.github.com>
2025-12-19 10:36:25 +0530

    Time: 1 ms (88.86%), Space: 45 MB (78.67%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,23 +1,51 @@
+// class Solution {
+//     public List<List<Integer>> permute(int[] nums) {
+//         List<List<Integer>> ans = new ArrayList<>();
+//         List<Integer> sol = new ArrayList<>();
+//         permute(nums, ans, sol);
+
+//         return ans;
+//     }
+
+//     public static void permute(int[] nums, List<List<Integer>> ans, List<Integer> sol){
+//         if(nums.length == sol.size()){
+//             ans.add(new ArrayList<>(sol));
+//             return;
+//         }
+
+//         for(int x : nums){
+//             if(!sol.contains(x)){
+//                 sol.add(x);
+//                 permute(nums, ans, sol);
+//                 sol.remove(sol.size()-1);
+//             }
+//         }
+//     }
+// }
+
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> sol = new ArrayList<>();
-        permute(nums, ans, sol);
-
+        boolean[] used = new boolean[nums.length]; // track visited elements
+        backtrack(nums, ans, new ArrayList<>(), used);
         return ans;
     }
 
-    public static void permute(int[] nums, List<List<Integer>> ans, List<Integer> sol){
-        if(nums.length == sol.size()){
+    private void backtrack(int[] nums, List<List<Integer>> ans, List<Integer> sol, boolean[] used) {
+        if (sol.size() == nums.length) {
             ans.add(new ArrayList<>(sol));
             return;
         }
 
-        for(int x : nums){
-            if(!sol.contains(x)){
-                sol.add(x);
-                permute(nums, ans, sol);
-                sol.remove(sol.size()-1);
+        for (int i = 0; i < nums.length; i++) {
+            if (!used[i]) {
+                used[i] = true;
+                sol.add(nums[i]);
+
+                backtrack(nums, ans, sol, used);
+
+                sol.remove(sol.size() - 1);
+                used[i] = false; // backtrack
             }
         }
     }
