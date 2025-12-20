353b5ee5de57254446349942ca9b92c31a690114
Dipak Bachhav <118241898+DipakBachhav25@users.noreply.github.com>
2025-12-20 10:31:45 +0530

    Time: 6 ms (94.72%), Space: 47.2 MB (23.84%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
@@ -2,17 +2,21 @@ class Solution {
     public int minDeletionSize(String[] strs) {
         int rows = strs.length;
         int cols = strs[0].length();
-        int count = 0;
+        // Precompute char grid to avoid repeated String.charAt calls
+        char[][] grid = new char[rows][cols];
+        for (int r = 0; r < rows; r++) {
+            grid[r] = strs[r].toCharArray();
+        }
 
-        for(int col=0; col<cols; col++){
-            for(int row=1; row<rows; row++){
-                if(strs[row].charAt(col) < strs[row-1].charAt(col)){
+        int count = 0;
+        for (int c = 0; c < cols; c++) {
+            for (int r = 1; r < rows; r++) {
+                if (grid[r][c] < grid[r - 1][c]) {
                     count++;
                     break;
                 }
             }
         }
-
         return count;
     }
 }
