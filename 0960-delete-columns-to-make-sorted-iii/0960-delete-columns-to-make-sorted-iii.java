eb7ca91cbf11c932a7e1eea44666942e13840301
Dipak Bachhav <118241898+DipakBachhav25@users.noreply.github.com>
2025-12-23 00:05:31 +0530

    Time: 6 ms (100%), Space: 46 MB (7.69%) - LeetHub

--- 0960-delete-columns-to-make-sorted-iii/0960-delete-columns-to-make-sorted-iii.java
+++ 0960-delete-columns-to-make-sorted-iii/0960-delete-columns-to-make-sorted-iii.java
@@ -1,32 +1,68 @@
 class Solution {
     public int minDeletionSize(String[] strs) {
-        int n = strs[0].length();
-        int m = strs.length;
+        // int n = strs[0].length();
+        // int m = strs.length;
 
-        boolean[][] canFollow = new boolean[n][n];
+        // boolean[][] canFollow = new boolean[n][n];
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = i + 1; j < n; j++) {
+        //         boolean ok = true;
+        //         for (int k = 0; k < m; k++) {
+        //             if (strs[k].charAt(i) > strs[k].charAt(j)) {
+        //                 ok = false;
+        //                 break;
+        //             }
+        //         }
+        //         canFollow[i][j] = ok;
+        //     }
+        // }
+
+        // int[] dp = new int[n];
+        // Arrays.fill(dp, 1);
+        // int maxLen = 1;
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < i; j++) {
+        //         if (canFollow[j][i]) {
+        //             dp[i] = Math.max(dp[i], dp[j] + 1);
+        //         }
+        //     }
+        //     maxLen = Math.max(maxLen, dp[i]);
+        // }
+
+        // return n - maxLen;
+
+        int m = strs.length;
+        int n = strs[0].length();
+
+        char[][] a = new char[m][n];
+        for (int i = 0; i < m; i++) {
+            a[i] = strs[i].toCharArray();
+        }
+
+        int[] dp = new int[n];
+        int maxLen = 0;
+
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
+            dp[i] = 1;
+
+            for (int j = 0; j < i; j++) {
                 boolean ok = true;
+
                 for (int k = 0; k < m; k++) {
-                    if (strs[k].charAt(i) > strs[k].charAt(j)) {
+                    if (a[k][j] > a[k][i]) {
                         ok = false;
                         break;
                     }
                 }
-                canFollow[i][j] = ok;
-            }
-        }
 
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);
-        int maxLen = 1;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (canFollow[j][i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                if (ok && dp[j] + 1 > dp[i]) {
+                    dp[i] = dp[j] + 1;
                 }
             }
-            maxLen = Math.max(maxLen, dp[i]);
+
+            if (dp[i] > maxLen) {
+                maxLen = dp[i];
+            }
         }
 
         return n - maxLen;
