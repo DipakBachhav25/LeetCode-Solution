5a0d19c438a0c4e7e4b2b19de2a092239fb3ec7c
Dipak Bachhav <118241898+DipakBachhav25@users.noreply.github.com>
2025-12-23 19:43:03 +0530

    Time: 40 ms (90.5%), Space: 174.8 MB (54%) - LeetHub

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.java
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.java
@@ -1,48 +1,75 @@
 class Solution {
     public int maxTwoEvents(int[][] events) {
         
+        // Arrays.sort(events, (a, b) -> a[0] - b[0]);
+        // int n = events.length;
+
+        // int[] tmp = new int[n + 1];
+        // for (int i = n - 1; i >= 0; i--) {
+        //     tmp[i] = Math.max(tmp[i + 1], events[i][2]);
+        // }
+
+        // int maxSum = 0;
+
+        // for (int[] row : events) {
+        //     int currentVal = row[2];
+        //     int currentEndTime = row[1];
+
+        //     int firstTrueIndex = binarySearch(events, currentEndTime);
+
+        //     if (firstTrueIndex != -1) {
+        //         currentVal += tmp[firstTrueIndex];
+        //     }
+
+        //     maxSum = Math.max(maxSum, currentVal);
+        // }
+
+        // return maxSum;
+
         Arrays.sort(events, (a, b) -> a[0] - b[0]);
-        int n = events.length;
 
-        int[] tmp = new int[n + 1];
-        for (int i = n - 1; i >= 0; i--) {
-            tmp[i] = Math.max(tmp[i + 1], events[i][2]);
-        }
+        // Max heap stores events by end time
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
 
+        int maxSingle = 0;   // best single event value seen so far
         int maxSum = 0;
 
-        for (int[] row : events) {
-            int currentVal = row[2];
-            int currentEndTime = row[1];
+        for (int[] e : events) {
+            int start = e[0], end = e[1], value = e[2];
 
-            int firstTrueIndex = binarySearch(events, currentEndTime);
-
-            if (firstTrueIndex != -1) {
-                currentVal += tmp[firstTrueIndex];
+            // Pop all events that end before current start
+            while (!pq.isEmpty() && pq.peek()[1] < start) {
+                maxSingle = Math.max(maxSingle, pq.poll()[2]);
             }
 
-            maxSum = Math.max(maxSum, currentVal);
+            // Combine current event with best non-overlapping
+            maxSum = Math.max(maxSum, value + maxSingle);
+
+            // Add current event to heap
+            pq.offer(e);
         }
 
         return maxSum;
 
     }
 
-    public int binarySearch(int[][] events, int endTime) {
-        int l = 0, r = events.length - 1;
-        int result = -1;
+    // public int binarySearch(int[][] events, int endTime) {
+    //     int l = 0, r = events.length - 1;
+    //     int result = -1;
 
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (events[mid][0] > endTime) {
-                result = mid;
-                r = mid - 1;
-            } else {
-                l = mid + 1;
-            }
-        }
+    //     while (l <= r) {
+    //         int mid = l + (r - l) / 2;
+    //         if (events[mid][0] > endTime) {
+    //             result = mid;
+    //             r = mid - 1;
+    //         } else {
+    //             l = mid + 1;
+    //         }
+    //     }
 
-        return result;
-    }
+    //     return result;
+    // }
+
+
 
 }
