f274d97be68c90013113f98964602711e44969eb
Dipak Bachhav <118241898+DipakBachhav25@users.noreply.github.com>
2026-01-06 12:41:34 +0530

    Time: 9 ms (64.42%), Space: 49.4 MB (17.48%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
@@ -17,29 +17,52 @@ class Solution {
     public int maxLevelSum(TreeNode root) {
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
-        q.add(null);
+        // q.add(null);
 
         int maxSum = Integer.MIN_VALUE;
-        int ans = 0;
-        int currSum = 0;
-        int currLevel = 0;
+        int ans = 1;
+        // int currSum = 0;
+        int currLevel = 1;
+
+        // This solution takes 11ms time
+        // while(!q.isEmpty()){
+        //     TreeNode currNode = q.remove();
+        //     if(currNode == null){
+        //         currLevel++;
+        //         if(currSum > maxSum){
+        //             maxSum = currSum;
+        //             ans = currLevel;
+        //         }
+        //         currSum = 0;
+        //         if(q.isEmpty()) break;
+        //         else q.add(null);
+        //     }else{
+        //         currSum += currNode.val;
+        //         if(currNode.left != null) q.add(currNode.left);
+        //         if(currNode.right != null) q.add(currNode.right);
+        //     }
+        // }
+
+        // return ans;
 
         while(!q.isEmpty()){
-            TreeNode currNode = q.remove();
-            if(currNode == null){
-                currLevel++;
-                if(currSum > maxSum){
-                    maxSum = currSum;
-                    ans = currLevel;
-                }
-                currSum = 0;
-                if(q.isEmpty()) break;
-                else q.add(null);
-            }else{
+            int size = q.size();
+            int currSum = 0;
+
+            for(int i=0; i<size; i++){
+                TreeNode currNode = q.poll();
                 currSum += currNode.val;
+
                 if(currNode.left != null) q.add(currNode.left);
                 if(currNode.right != null) q.add(currNode.right);
             }
+
+            if(currSum > maxSum){
+                maxSum = currSum;
+                ans = currLevel;
+            }
+
+            currLevel++;
         }
 
         return ans;
