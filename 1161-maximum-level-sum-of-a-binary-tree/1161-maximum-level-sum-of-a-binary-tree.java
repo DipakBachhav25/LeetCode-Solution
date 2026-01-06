f2e69ca39a845cafabc15cc78863a41a751b2a70
Dipak Bachhav <118241898+DipakBachhav25@users.noreply.github.com>
2026-01-06 12:42:25 +0530

    Time: 8 ms (93.82%), Space: 49 MB (72.92%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
@@ -45,6 +45,7 @@ class Solution {
 
         // return ans;
 
+        // This solution takes 9ms time which is better than above solution
         while(!q.isEmpty()){
             int size = q.size();
             int currSum = 0;
