6dd699ad9aa5f7d3ec64180cfe88c191867ecace
Dipak Bachhav <118241898+DipakBachhav25@users.noreply.github.com>
2026-01-14 01:06:00 +0530

    Time: 132 ms (42.63%), Space: 145.3 MB (80.53%) - LeetHub

--- 3453-separate-squares-i/3453-separate-squares-i.java
+++ 3453-separate-squares-i/3453-separate-squares-i.java
@@ -1,24 +1,50 @@
 class Solution {
     public double separateSquares(int[][] squares) {
-        double l = Double.MAX_VALUE, h = Double.MIN_VALUE;
+        // double l = Double.MAX_VALUE, h = Double.MIN_VALUE;
 
-        for(int[] sq : squares){
+        // for(int[] sq : squares){
+        //     l = Math.min(l, sq[1]);
+        //     h = Math.max(h, sq[1]+sq[2]);
+        // }
+
+        // for(int i=0; i<60; i++){
+        //     double mid = (l+h)/2, d = 0;
+        //     for(int[] sq : squares){
+        //         double y = sq[1], line = sq[2], t = y+line;
+        //         if(mid <= y) d += line*line;
+        //         else if(mid >= t) d -= line*line;
+        //         else d += (t-mid)*line - (mid-y)*line;
+        //     }
+        //     if(d > 0) l = mid;
+        //     else h = mid;
+        // }
+
+        // return l;
+
+        double l = Double.MAX_VALUE, h = Double.MIN_VALUE;
+        int n = squares.length;
+        double[] areas = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            int[] sq = squares[i];
             l = Math.min(l, sq[1]);
-            h = Math.max(h, sq[1]+sq[2]);
+            h = Math.max(h, sq[1] + sq[2]);
+            areas[i] = (double) sq[2] * sq[2];
         }
 
-        for(int i=0; i<60; i++){
-            double mid = (l+h)/2, d = 0;
-            for(int[] sq : squares){
-                double y = sq[1], line = sq[2], t = y+line;
-                if(mid <= y) d += line*line;
-                else if(mid >= t) d -= line*line;
-                else d += (t-mid)*line - (mid-y)*line;
+        for (int iter = 0; iter < 60; iter++) {
+            double mid = (l + h) / 2, d = 0;
+            for (int i = 0; i < n; i++) {
+                int[] sq = squares[i];
+                double y = sq[1], line = sq[2], t = y + line;
+                if (mid <= y) d += areas[i];
+                else if (mid >= t) d -= areas[i];
+                else d += (t - mid) * line - (mid - y) * line;
             }
-            if(d > 0) l = mid;
+            if (d > 0) l = mid;
             else h = mid;
         }
-
         return l;
+
     }
 }
