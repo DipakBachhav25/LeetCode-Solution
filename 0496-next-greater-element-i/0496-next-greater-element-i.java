fc21ce655653df9475d9b01112db063896a6df21
Dipak Bachhav <118241898+DipakBachhav25@users.noreply.github.com>
2026-01-19 22:57:46 +0530

    Time: 3 ms (92.3%), Space: 45.3 MB (52.9%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,32 +1,50 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        // int n = nums2.length;
+        // Stack<Integer> st = new Stack<>();
+        // Map<Integer, Integer> map = new HashMap<>();
+        // int[] tmp = new int[n];
+        // st.push(n-1);
+        // tmp[n-1] = -1;
+        // map.put(nums2[n-1], n-1);
+
+        // for(int i=n-2; i>=0; i--){
+        //     int curr = nums2[i];
+        //     map.put(curr, i);
+
+        //     while(!st.isEmpty() && curr >= nums2[st.peek()]){
+        //         st.pop();
+        //     }
+
+        //     if(st.isEmpty()){
+        //         tmp[i] = -1;
+        //     }else{
+        //         tmp[i] = nums2[st.peek()];
+        //     }
+        //     st.push(i);
+        // }
+
+        // for(int i=0; i<nums1.length; i++){
+        //     int t = nums1[i];
+        //     nums1[i] = tmp[map.get(t)];
+        // }
+
+        // return nums1;
+
         int n = nums2.length;
-        Stack<Integer> st = new Stack<>();
-        Map<Integer, Integer> map = new HashMap<>();
-        int[] tmp = new int[n];
-        st.push(n-1);
-        tmp[n-1] = -1;
-        map.put(nums2[n-1], n-1);
+        Map<Integer, Integer> nextGreater = new HashMap<>(n*2);
+        Deque<Integer> st = new ArrayDeque<>();
 
-        for(int i=n-2; i>=0; i--){
-            int curr = nums2[i];
-            map.put(curr, i);
-
-            while(!st.isEmpty() && curr >= nums2[st.peek()]){
-                st.pop();
+        for(int num : nums2){
+            while(!st.isEmpty() && num > st.peek()){
+                nextGreater.put(st.pop(), num);
             }
-
-            if(st.isEmpty()){
-                tmp[i] = -1;
-            }else{
-                tmp[i] = nums2[st.peek()];
-            }
-            st.push(i);
+            st.push(num);
         }
+        while(!st.isEmpty()) nextGreater.put(st.pop(), -1);
 
         for(int i=0; i<nums1.length; i++){
-            int t = nums1[i];
-            nums1[i] = tmp[map.get(t)];
+            nums1[i] = nextGreater.get(nums1[i]);
         }
 
         return nums1;
