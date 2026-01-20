a0801c04f00b899568dc92c61309499b7fddb5b2
Dipak Bachhav <118241898+DipakBachhav25@users.noreply.github.com>
2026-01-20 12:25:48 +0530

    Time: 1 ms (100%), Space: 46.6 MB (85.96%) - LeetHub

--- 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.java
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.java
@@ -1,33 +1,14 @@
-// class Solution {
-//     public int[] minBitwiseArray(List<Integer> nums) {
-//         int n = nums.size();
-//         int[] ans = new int[n];
-
-//         for (int i = 0; i < n; i++) {
-//             int curr = nums.get(i);
-//             if (curr == 2) {
-//                 ans[i] = -1;
-//             } else {
-//                 ans[i] = curr - Integer.lowestOneBit(curr);
-//             }
-//         }
-
-//         return ans;
-
-//     }
-// }
-
 class Solution {
     public int[] minBitwiseArray(List<Integer> nums) {
         int n = nums.size();
-        int[] result = new int[n];
+        int[] ans = new int[n];
 
         for (int i = 0; i < n; i++) {
             int y = nums.get(i);
 
             // Special case: no valid answer exists for 2
             if (y == 2) {
-                result[i] = -1;
+                ans[i] = -1;
                 continue;
             }
 
@@ -36,9 +17,9 @@ class Solution {
             int p = Integer.numberOfTrailingZeros(~shifted); // count of trailing ones in (y >> 1)
 
             // Flip bit (p) in y (which is bit (j-1) with j = p + 1)
-            result[i] = y ^ (1 << p);
+            ans[i] = y ^ (1 << p);
         }
 
-        return result;
+        return ans;
     }
 }
