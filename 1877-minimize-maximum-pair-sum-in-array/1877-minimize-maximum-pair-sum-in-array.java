538f10fbfd3edc7e6553e6c780cffe052e52e5f6
Dipak Bachhav <118241898+DipakBachhav25@users.noreply.github.com>
2026-01-24 23:27:30 +0530

    Time: 8 ms (98.78%), Space: 99.3 MB (5.16%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.java
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.java
@@ -1,13 +1,43 @@
 class Solution {
     public int minPairSum(int[] nums) {
-        Arrays.sort(nums);
-        int l = 0;
-        int r = nums.length-1;
+        // Arrays.sort(nums);
+        // int l = 0;
+        // int r = nums.length-1;
+        // int ans = 0;
+
+        // while(l < r){
+        //     ans = Math.max(ans, (nums[l]+nums[r]));
+        //     l++; r--;
+        // }
+
+        // return ans;
+
+        // The above solution takes O(nlogn)
+
+        int[] freq = new int[100001];
+        int minVal = Integer.MAX_VALUE;
+        int maxVal = Integer.MIN_VALUE;
+
+        for(int n : nums){
+            freq[n]++;
+            minVal = Math.min(minVal, n);
+            maxVal = Math.max(maxVal, n);
+        }
+
+        int low = minVal;
+        int high = maxVal;
         int ans = 0;
 
-        while(l < r){
-            ans = Math.max(ans, (nums[l]+nums[r]));
-            l++; r--;
+        while(low <= high){
+            if(freq[low] == 0) low++;
+            else if(freq[high] == 0) high--;
+            else{
+                ans = Math.max(ans, low+high);
+                int pairsCount = (low == high) ? freq[low]/2 : Math.min(freq[low], freq[high]);
+
+                freq[low] -= pairsCount;
+                freq[high] -= pairsCount;
+            }
         }
 
         return ans;
